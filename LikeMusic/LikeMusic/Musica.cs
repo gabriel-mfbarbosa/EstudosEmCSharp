c6fcab1c84d9b9bbabb129d1f9ae79a7fdc6e8a7
Gabriel Barbosa <gabriel.mfbarbosa@gmail.com>
2025-01-23 15:46:34 -0300

    Add files via upload
    Arquivos referente ao projeto desenvolvido junto ao curso da Alura - C#: aplicando a Orientação a Objetos

---
+++ LikeMusic/LikeMusic/Musica.cs
@@ -0,0 +1,36 @@
+﻿class Musica
+{
+    public Musica(Banda artista, string nome, Genero genero)
+    {
+        Artista = artista;
+        Nome = nome;
+        Genero = genero;
+    }
+
+    public string Nome {  get; }
+    public Genero Genero { get; }
+    public Banda Artista {  get; }
+    public int Duracao {  get; set; }
+    public bool Disponivel { get; set; }
+    public string DescricaoResumida =>
+        $"A música {Nome} pertece a banda {Artista}";
+    
+
+
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {Nome}");
+        Console.WriteLine($"Artista: {Artista.Nome}");
+        Console.WriteLine($"Duração: {Duracao}");
+        Console.WriteLine($"Gênero: {Genero.Nome}");
+
+        if (Disponivel)
+        {
+            Console.WriteLine("Disponível no plano.");
+        }
+        else
+        {
+            Console.WriteLine("Adquira o plano Plus+");
+        }
+    }
+}
---
+++ LikeMusic/LikeMusic/Program.cs
@@ -0,0 +1,47 @@
+﻿//Banda linkinPark = new Banda("Linkin Park");
+
+//Album albumDoLinkinPark = new Album("Hybrid Theory");
+
+//Musica musica1 = new(linkinPark, "Papercut", new Genero { Nome = "Rock" })
+//{
+//    Duracao = 185,
+//    Disponivel = true,
+//};
+
+//Musica musica2 = new Musica(linkinPark, "With you", new Genero { Nome = "Rock" })
+//{
+//    Duracao = 203,
+//    Disponivel = false,
+//};
+
+//albumDoLinkinPark.AdicionarMusica(musica1);
+//albumDoLinkinPark.AdicionarMusica(musica2);
+//linkinPark.AdicionarAlbum(albumDoLinkinPark);
+
+//linkinPark.ExibirDiscografia();
+//Console.WriteLine("\n");
+//albumDoLinkinPark.ExibirMusicasDoAlbum();
+//Console.WriteLine("\n");
+//musica1.ExibirFichaTecnica();
+//Console.WriteLine("\n");
+//musica2.ExibirFichaTecnica();
+
+
+//****************************************************************************************************
+// A partir daqui é o desafio
+//****************************************************************************************************
+
+
+Episodio ep1 = new(1, "Técnicas de facilitação", 45);
+ep1.AdicionarConvidados("José Leandro");
+ep1.AdicionarConvidados("Ricardo Augusto");
+
+Episodio ep2 = new(2, "Técnicas de aprendizado", 60);
+ep2.AdicionarConvidados("Ronaldo");
+ep2.AdicionarConvidados("Elias");
+ep2.AdicionarConvidados("Marcella");
+
+Podcast podcast = new("Podcast Facilitadores", "Gabriel");
+podcast.AdicionarEpisodio(ep1);
+podcast.AdicionarEpisodio(ep2);
+podcast.ExibirInformacoes();
